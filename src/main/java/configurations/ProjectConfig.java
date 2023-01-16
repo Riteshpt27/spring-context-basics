9ed522d4fe9995550f4ae66e28dd2252169eb88b
RPurohit2 <rpurohit2@slb.com>
2023-01-16 14:49:59 +0530

    Creating mulitple beans of same type, defining a bean as primary

--- src/main/java/configurations/ProjectConfig.java
+++ src/main/java/configurations/ProjectConfig.java
@@ -3,17 +3,32 @@ package configurations;
 import main.Parrot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    Parrot parrot() {
+    Parrot parrot1() {
         Parrot p = new Parrot();
         p.setName("koko");
         return p;
     }
 
+    @Bean
+    Parrot parrot2() {
+        Parrot p = new Parrot();
+        p.setName("miki");
+        return p;
+    }
+
+    @Bean
+    @Primary
+    Parrot parrot3() {
+        Parrot p = new Parrot();
+        p.setName("riki");
+        return p;
+    }
     @Bean
     String hello() {
         return "hello";
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,11 +10,5 @@ public class Main {
 
         Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
-
-        String s = context.getBean(String.class);
-        System.out.println(s);
-
-        Integer n = context.getBean(Integer.class);
-        System.out.println(n);
     }
 }
